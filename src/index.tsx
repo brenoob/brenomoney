b74c2e3a0342c8cb05ec8074310160223f883151
brenoob <57812022+brenoob@users.noreply.github.com>
2021-10-16 19:07:17 -0300

    Listando transações e seeds e Formatando valores

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,28 @@ createServer({
   models: {
     transaction: Model,
   },
-  
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-10-14 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel casa',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 900,
+          createdAt: new Date('2021-10-16 11:00:00'),
+        }
+      ]
+    })
+  },
 
   routes() {
     this.namespace = "api";
