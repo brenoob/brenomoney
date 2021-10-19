106fba4ab652b1c24031253b02498ff440bcc841
brenoob <57812022+brenoob@users.noreply.github.com>
2021-10-19 20:02:47 -0300

    Carregando transações, Movendo criação para o context e Finalizando inserção da transação

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
 import EntradaImg from '../../assets/entradas.svg';
 import SaidaImg from '../../assets/saidas.svg';
 import TotalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from "./styles";
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
+
+    console.log(transactions)
+
     return(
         <Container>
             <div>
